226ce3ba6a299489028244d5559bef9b1f81902e
Teresa Heidt <teresaheidt@hotmail.com>
2020-07-30 17:41:05 -0500

    save to local storage

--- calendar.js
+++ calendar.js
@@ -1,4 +1,5 @@
 // to show the day and date at the top of the page
+
 let d = new Date()
 document.getElementById("currentDay").innerHTML = d.toString();
 
@@ -6,7 +7,7 @@ document.getElementById("currentDay").innerHTML = d.toString();
 
 var textboxArray = $(".form-control")
 
-textboxArray.forEach(function(myElement){{
+$(textboxArray).each(function(myElement){{
     if ( this.moment === "ago" ) {
     this.style.color = ".past";
     } 
@@ -20,10 +21,45 @@ textboxArray.forEach(function(myElement){{
     };
 });
 
+//text box array
+for (let i = 0; i < textboxArray.length; i++) {
+    let val = getCookie("box" + i);
+    textboxArray[i].value = val != null ? val : "";
+  }
+   
+//   document.getElementById("date").innerHTML = moment();
+   
+  console.log(moment())
+  function test(box) {
+    setCookie("box" + box, textboxArray[box].value);
+  }
+
+  function setCookie(name, value) {
+    document.cookie = name + "=" + value + ";path=/;";
+
+  }
+   
+  function getCookie(name) {
+    var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+    return v ? v[2] : null;
+  }
+
 // Save message in the timeblock
 
 
 // Save to local storage
+const button = document.querySelector(".button-addon2");
+const formControl = document.querySelector(".form-control");
+
+btn.onclick = function () {
+  const formControl = formControl.value;
+  console.log(formControl);
+
+};
+
+localStorage.setItem("date", "event");
+console.log("name")
+
 
 
 // Persist the event
